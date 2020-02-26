53009e3fc778be6ef1e1d26a8e3bc8a48e291a6a
vipul.jain <vipul.jain@affinerobotics.com>
2020-02-26 04:11:48 -0800

    Periodic submit Added performace monitoring Refactored code etc.

--- Vision/classification/train/classnet_train.py
+++ Vision/classification/train/classnet_train.py
@@ -1,11 +1,10 @@
 from Affine.Vision.classification.src.darknet53 import darknet
-from dataset_utils import load_imagenet_data as load_data, load_imagenet_val as load_val
+from dataset_utils import load_imagenet_val as load_val
 from dataset_utils import data_prefetcher
 from train_utils import parse_args, AverageMeter, ProgressMeter, Config, setup_and_launch
 
 import os, time, datetime
 import warnings
-
 import math
 import torch
 import torch.nn as nn
@@ -26,27 +25,27 @@ except:
 HTIME = lambda t: time.strftime( "%H:%M:%S", time.gmtime( t ) )
 
 def main_worker( gpu, args, config ):
-    best_acc1 = 0
-
     torch.backends.cudnn.benchmark = True
     torch.backends.cudnn.enabled = True
     
+    best_acc1 = 0    
     args.writer = None
-    if args.gpu or gpu % args.gpus_per_node == 0:
+    distributed = not args.gpu
+
+    if not distributed or gpu % args.gpus_per_node == 0:
         args.writer = SummaryWriter( filename_suffix="{}".format( gpu ) )
 
-    if args.gpu is None:
+    if distributed:
         dist.init_process_group( backend=args.dist_backend, 
                                  init_method="tcp://10.0.1.164:12345", 
                                  world_size=args.world_size, rank=gpu )
-
         print( "Process: {}, rank: {}, world_size: {}".format( gpu, dist.get_rank(), dist.get_world_size() ) )
 
     # Set the default device, any tensors created by cuda by 'default' will use this device
     torch.cuda.set_device( gpu )
 
-    train_loader = load_data( config, args, gpu )
-    val_loader = load_val( config, args )
+    train_loader = load_data( config.train_path, args, distributed )
+    val_loader = load_val( config.val_path, args, distributed )
     assert train_loader.dataset.classes == val_loader.dataset.classes
 
     model = darknet()
@@ -63,7 +62,7 @@ def main_worker( gpu, args, config ):
     # https://github.com/NVIDIA/apex/tree/master/examples/imagenet
     model, optimizer = amp.initialize( model, optimizer, opt_level="O1" )
 
-    if args.gpu is None:
+    if distributed:
         # By default, apex.parallel.DistributedDataParallel overlaps communication 
         # with computation in the backward pass.
         # delay_allreduce delays all communication to the end of the backward pass.
@@ -87,12 +86,12 @@ def main_worker( gpu, args, config ):
     end_epoch = start_epoch + args.epochs
 
     for epoch in range( start_epoch, end_epoch ):
-        if args.gpu is None:
+        if distributed:
             train_loader.sampler.set_epoch( epoch )
         
         train_or_eval( True, gpu, train_loader, model, criterion, optimizer, args, epoch )
 
-        if args.gpu or gpu % args.gpus_per_node == 0:
+        if gpu % args.gpus_per_node == 0 or not distributed:
             acc1 = train_or_eval( False, gpu, val_loader, model, criterion, None, args, 0 )
 
             is_best = acc1 > best_acc1
@@ -110,10 +109,8 @@ def main_worker( gpu, args, config ):
 
 
 def train_or_eval( train, gpu, loader, model, criterion, optimizer, args, epoch ):
-    if train:
-        model.train()
-    else:
-        model.eval()
+    phase = "train" if train else "test"
+    model.train() if train else model.eval()
 
     losses = AverageMeter( "Loss", ":.4e" )
     top1 = AverageMeter( "Accuracy1", ":6.2f" )
@@ -121,29 +118,34 @@ def train_or_eval( train, gpu, loader, model, criterion, optimizer, args, epoch 
     prefix = "Epoch:[{}]".format( epoch + 1 ) if train else "Test: "
     progress = ProgressMeter( len( loader ), [ losses, top1, top5 ], prefix=prefix )
 
-    prefetcher = data_prefetcher( loader )
-    
-    total_time = [ 0.0 ] * 5
-    time0 = time_init = time.time()
 
+    # The timing infrastructure below is not very accurate relatively, due to asynchronous 
+    # GPU operation. However, it is very lightweight and can be left working all the time 
+    # for a relative estimation of how the training loop is performing.
+    markers = ( "Time loading data:", "Time forward:", "Time backward:" )
+    t = torch.zeros( [ len( markers ) ], dtype=float )
+    total = torch.zeros( [ len( markers ) - 1 ], dtype=float )
+    
+    if args.prof:
+        print( "Profiling started" )
+        torch.cuda.cudart().cudaProfilerStart()
+
+    t_init = t[0] = time.time()
+    prefetcher = data_prefetcher( loader )
     with torch.set_grad_enabled( mode=train ):
         for i, ( images, target ) in enumerate( prefetcher ):
-            images = images.cuda( gpu, non_blocking=True )
-            target = target.cuda( gpu, non_blocking=True )
-            time_dataload = time.time()
+            t[1] = time.time()
+            if args.prof: torch.cuda.nvtx.range_push( "Prof start iteration {}".format( i ) )
 
+            if args.prof: torch.cuda.nvtx.range_push( "forward" )
             output = model( images )
+            if args.prof: torch.cuda.nvtx.range_pop()
+
             loss = criterion( output, target )
-            time_forward = time.time()
 
-            batch_size = images.size( 0 )
-            acc1, acc5 = accuracy( output, target, topk=( 1, 5 ) )
-            losses.update( loss.item(), batch_size )
-            top1.update( acc1[ 0 ], batch_size )
-            top5.update( acc5[ 0 ], batch_size )
+            t[2] = time.time()
+            t[3] = time.time()
 
-            time_loss_update = time.time()
-            time_backward = time.time()
             # All the code that needs to run only when training goes here
             if train:
                 n_iter = epoch * len( loader ) + i
@@ -151,52 +153,66 @@ def train_or_eval( train, gpu, loader, model, criterion, optimizer, args, epoch 
 
                 optimizer.zero_grad()
                 
+                if args.prof: torch.cuda.nvtx.range_push( "backward" )
                 with amp.scale_loss( loss, optimizer ) as scaled_loss:
                     scaled_loss.backward()
-                
+                if args.prof: torch.cuda.nvtx.range_pop()
+
+                if args.prof: torch.cuda.nvtx.range_push( "optimizer step" )
                 optimizer.step()
-                time_backward = time.time()
+                if args.prof: torch.cuda.nvtx.range_pop()
+                t[3] = time.time()
+            else:
+                top1.update( acc1[0], images.size(0) )
 
-                if ( i % 100 == 0 ) and ( gpu % args.gpus_per_node == 0 ):
-                    args.writer.add_scalar( "Loss/train/gpu{}".format( gpu ), loss.item(), n_iter )
-                    args.writer.add_scalar( "Accuracy/train/gpu{}".format( gpu ), acc1, n_iter )
-                    args.writer.add_scalar( "Loss/Accuracy", acc1, lr * 10000 )
-            # End of training specific code
-            time_misc = time.time()            
+            total += t[ 1: ] - t[ :-1 ]
 
-            time_markers = [ time_init, time_dataload, time_forward, time_loss_update, time_backward, time_misc ]
-            time_delta = time_markers[ 1: ] - time_markers[ :-1 ]
-            total_time += time_delta
+            if  i % 100 == 0 and gpu % args.gpus_per_node == 0:
+                acc1, acc5 = accuracy( output.detach(), target, topk=( 1, 5 ) )
+                losses.update( loss.item(), images.size(0) )
+                top1.update( acc1[0], images.size(0) )
+                top5.update( acc5[0], images.size(0) )
 
-            if  i % 200 == 0 and gpu % args.gpus_per_node == 0:
+                args.writer.add_scalar( "Loss/{}".format( phase ), loss.item(), n_iter )
+                args.writer.add_scalar( "Accuracy/{}".format( phase ), acc1, n_iter )
+                args.writer.add_scalar( "Loss/Accuracy", acc1, lr * 10000 )
+
                 progress.display( i )
-                for x, y in ( ( "Time loading data:", HTIME( total_time[ 0 ] ) ),
-                              ( "Time forward:", HTIME( total_time[ 1 ] ) ),
-                              ( "Time loss update:", HTIME( total_time[ 2 ] ) ),
-                              ( "Time backward:", HTIME( total_time[ 3 ] ) ),
-                              ( "Time misc:", HTIME( total_time[ 4 ] ) )
-                            ):
-                    print( "{:<25s}{}".format( x, y ) )
+                for x, y in zip( markers, total ):
+                    print( "{:<25s}{}".format( x, HTIME( y ) ) )
                 print()
-            time_init = time.time()
-            
-    print( "Total epoch time: {}".format( HTIME( time.time() - time0 ) ) )
-    return top1.avg
 
+            t[0] = time.time()
+            if args.prof: torch.cuda.nvtx.range_pop()
+            if args.prof and i == 20:
+                break
+
+    print( "Total {} epoch time: {}".format( phase, HTIME( time.time() - t_init ) ) )
+    if args.prof:
+        print( "Profiling stopped" )
+        torch.cuda.cudart().cudaProfilerStop()
+
+    return top1.avg
 
 def accuracy( outputs, targets, topk=(1, ) ):
     with torch.no_grad():
         maxk = max( topk )
         batch_size = targets.size( 0 )
-        _, topk_classes = outputs.topk( maxk, dim=1, largest=True, sorted=True )
-        topk_classes = topk_classes.t()
-        correct = topk_classes.eq( targets.expand_as( topk_classes ) )
-
+        _, topk_idx = outputs.topk( maxk, dim=1, largest=True, sorted=True )
+        topk_idx = topk_idx.t()
+        correct = topk_idx.eq( targets.expand_as( topk_idx ) )
         res = []
         for k in topk:
             correct_k = correct[ :k ].view( -1 ).float().sum( 0, keepdim=True )
             res.append( correct_k.mul_( 100.0 / batch_size ) )
-        return res
+    return res
+
+def accuracy_with_score( outputs, targets ):
+    with torch.no_grad():
+        b = targets.size( 0 )
+        _, idx = outputs.topk( 1, dim=1, largest=True, sorted=True )
+        idx = idx.t()
+        correct = idx.eq( targets.expand_as( idx ) )
 
 def adjust_learning_rate( optimizer, i, args, policy="triangle" ):
     """learning rate schedule
