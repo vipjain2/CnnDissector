372bb6a89fd15f19e3b813f05d27d54c2e9a446e
vipul.jain <vipul.jain@affinerobotics.com>
2020-02-27 04:31:54 -0800

    Periodic commit

--- Vision/classification/train/validate.py
+++ Vision/classification/train/validate.py
@@ -7,46 +7,103 @@ import os, time, datetime
 import warnings
 import torch
 import torch.nn as nn
+from collections import OrderedDict
+
+HTIME = lambda t: time.strftime( "%H:%M:%S", time.gmtime( t ) )
+
+score = torch.zeros( [ 1000 ], dtype=int )
+present = torch.zeros( [ 1000 ], dtype=int )
 
 def validate( loader, model, args ):
-    model.eval()
+    global score, present
+
+    if args.gpu is not None:
+        print( "** Running on GPU{}".format( args.gpu ) )
+        torch.cuda.set_device( args.gpu )
+        model.eval().cuda( args.gpu )
+        score = score.cuda( args.gpu )
+        present = present.cuda( args.gpu )
+    else:
+        model.eval().cpu()
 
     top1 = AverageMeter( "Accuracy1", ":6.2f" )
     prefix = "Epoch:[{}]".format( 1 )
     progress = ProgressMeter( len( loader ), [ top1 ], prefix=prefix )
 
     with torch.set_grad_enabled( mode=False ):
-        for i, ( images, target ) in enumerate( loader ):
+        for i, ( images, targets ) in enumerate( loader ):
+            if args.gpu is not None:
+                images = images.cuda( args.gpu, non_blocking=True )
+                targets = targets.cuda( args.gpu, non_blocking=True )
 
+            t0 = time.time()
             output = model( images )
-            acc1 = accuracy_with_score( output.detach(), target )
-            top1.update( acc1[0], images.size(0) )
+            t1 = time.time()
+            accuracy_with_score( output.detach(), targets )
+            t2 = time.time()
 
             if  i % 50 == 0:
                 progress.display( i )
+                print( "{:<20s}\t{}".format( "forward", HTIME( t1 - t0 ) ) )
+                print( "{:<20s}\t{}".format( "accuracy", HTIME( t2 - t1 ) ) )
+
+    non_zero = present.nonzero().squeeze( 1 )
+
+    present = present[ non_zero ]
+    score = score[ non_zero ]
+    score = score.float().div( present )
+    score = score.mul_( 100 )
+
+    print( "\nBest scores" )
+    best, ids = score.topk( 5, dim=0, largest=True, sorted=True )
+    for i, p in zip( ids, best ):
+        print( "{:<10d}\t{:4.1f}".format( i.data, p.data ) )
+
+    print( "\nWorst scores" )
+    last, ids = score.topk( 5, dim=0, largest=False, sorted=True )
+    for i, p in zip( ids, last ):
+        print( "{:<10d}\t{:4.1f}".format( i.data, p.data ) )
 
 
 def accuracy_with_score( outputs, targets ):
+    global present, score
     with torch.no_grad():
-        b = targets.size( 0 )
+        i, c = torch.unique( targets, return_counts=True )
+        present[ i ] += c
+
         _, idx = outputs.topk( 1, dim=1, largest=True, sorted=True )
         idx = idx.t()
         correct = idx.eq( targets.expand_as( idx ) )
-        
-        correct_1 = correct[ :1 ].view( -1 ).float().sum( 0, keepdim=True )
-        return correct_1.mul_( 100.0 / b )
+
+        positives = torch.masked_select( idx, correct )
+        i, c = torch.unique( positives, return_counts=True )
+        score[ i ] += c
+
+def load_checkpoint( model, checkpoint_path ):
+    print( "Loading checkpoint {}".format( checkpoint_path ) )
+
+    checkpoint = torch.load( checkpoint_path, map_location='cpu' )
+    state_dict = checkpoint[ "model" ]
+    attempt = 0
+    while attempt < 2:
+        try:
+            model.load_state_dict( state_dict )
+        except RuntimeError:
+            attempt += 1
+            state_dict = OrderedDict( [ ( k[ 7: ], v ) for k, v in state_dict.items() 
+                                                                if k.startswith( "module" ) ] )
+        else:
+            break
 
 def main():
     args = parse_args()
-    checkpoint = os.path.join( config.checpoint_path, config.checkpoint_name )
     loader = load_val( config.val_path, args, distributed=False )
 
     model = darknet()
 
-    print( "Loading checkpoint {}".format( checkpoint ) )
-    checkpoint = torch.load( checkpoint, map_location='cpu' )
-    model.load_state_dict( checkpoint[ "model" ] )
-    del checkpoint
+    checkpoint_path = os.path.join( config.checkpoint_path, config.checkpoint_name )
+    load_checkpoint( model, checkpoint_path )
+    validate( loader, model, args )
 
 
 if __name__ == "__main__":
