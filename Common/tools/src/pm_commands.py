6da0fe65338f0fd03a6a5dd65e862db06c90fba5
vipul.jain <vipjain2@gmail.com>
2025-11-06 00:52:38 -0800

    Refactor command methods into separate pm_commands module
    1. Created pm_commands.py to hold command methods that don't use global variables or decorators
    2. Moved 12 command methods from pmshell to pm_commands.py:
       - do_quit
       - do_summary
       - do_nparams
       - do_show_config
       - do_show_image
       - do_visualizer
       - do_up
       - do_down
       - do_set_class
       - do_set_compare
       - do_set_dataset
       - do_dataset_suffix
    3. Kept methods using global variables or @supports_compare decorator in pmshell
    4. Shell class inherits from Commands class using multiple inheritance to access moved methods
    5. Removed unused imports from pm_commands.py (numpy, torch, PIL, etc.)

---
+++ Common/tools/src/pm_commands.py
@@ -0,0 +1,165 @@
+import os, sys
+from functools import reduce
+from pm_helper_classes import Dataset
+from layer_visualizer import LayerVisualizer
+
+
+class Commands:
+    """Class containing command methods for the Shell that don't use global variables or decorators"""
+
+    def do_quit( self, args ):
+        """Exits the shell
+        """
+        self.message( "Exiting shell" )
+        self.close()
+        raise SystemExit
+
+
+    def do_summary( self, args ):
+        """Prints pytorch model summary
+        """
+        model_info, _ = self.get_info_from_context( args )
+        if model_info is None:
+            self.message( "Model not found. Please set the model in context first" )
+            return
+
+        model = model_info.model
+        try:
+            self.message( "Model: {}".format( model_info.name ) )
+            self.message( "-" * 60 )
+            for name, module in model.named_modules():
+                if name:  # Skip the root module
+                    self.message( "{:<40} {}".format( name, module.__class__.__name__ ) )
+        except:
+            self.error( sys.exc_info()[ 1 ] )
+
+    do_show_summary = do_summary
+
+
+    def do_nparams( self, args ):
+        """Print the total number of parameters in a model
+        """
+        model_info, _  = self.get_info_from_context( args )
+        if model_info is None:
+            self.message( "Model \"{}\" not found. Please set the model in context first".format( args ) )
+            return
+
+        model = model_info.model
+        n =  sum( reduce( lambda x, y: x * y, p.size() ) for p in model.parameters())
+        print( "{:,}".format( n ) )
+
+    do_nparam = do_nparams
+    do_show_nparams = do_nparams
+
+
+    def do_show_config( self, args ):
+        """Display the current configuration settings
+        Usage: show config
+        """
+        self.message( "Configuration settings:" )
+        config_attrs = [ attr for attr in dir( self.config ) if not attr.startswith( '_' ) ]
+
+        if not config_attrs:
+            self.message( "  No configuration attributes set" )
+        else:
+            for attr in sorted( config_attrs ):
+                value = getattr( self.config, attr )
+                self.message( "  {:<20} = {}".format( attr, value ) )
+
+    do_show_conf = do_show_config
+
+
+    def do_show_image( self, args ):
+        """Display an image array:
+        Usage: show image [ image_var ]
+
+        If an (optional) image_var is specified, it's considered as a global
+        variable referencing an image array and tries to show this image.
+        If no args are specified, show the image referenced by the
+        global "image" variable.
+        """
+        if not args:
+            args = "image"
+        img = self.in_place_eval( args )
+        if img is None:
+            self.error( "Could not display image" )
+            return
+
+        if not self.fig.imshow( img ):
+            self.error( "Unsupported image type" )
+            return
+
+    do_show_img = do_show_image
+
+
+    def do_visualizer( self, args ):
+        if self.cur_model is None:
+            self.error( "No current model set in context" )
+            return
+        image = self.load_from_global( "image" )
+        if image is None:
+            self.error( "Please load an input image first" )
+            return
+
+        visual = LayerVisualizer( self.cur_model )
+        visual.set_image( image )
+        visual.start()
+
+
+    def do_up( self, args ):
+        if not self.cur_model:
+            self.error( "Please load a model first" )
+            return
+
+        if not self.cur_model.up():
+            self.message( "Already at top" )
+        id, layer = self.cur_model.get_cur_id_layer()
+        self.message( "Current layer is {}: {}".format( id, layer ) )
+
+
+    def do_down( self, args ):
+        if not self.cur_model:
+            self.error( "Please load a model first" )
+            return
+        if not self.cur_model.down():
+            self.message( "Already at bottom" )
+        id, layer = self.cur_model.get_cur_id_layer()
+        self.message( "Current layer is {}: {}".format( id, layer ) )
+
+
+    def do_set_class( self, args ):
+        if self.dataset is None:
+            self.error( "No dataset is configured" )
+        idx = int( args ) if args else 0
+        if not self.dataset.set_class( idx ):
+            self.error( "Could not set class to {}".format( args ) )
+
+
+    def do_set_compare( self, args ):
+        if not args:
+            self.compare = None
+        elif args not in self.models and args not in ( "image", "flw" ):
+            self.error( "Model \"{}\" not in context".format( args ) )
+            return
+        else:
+            self.compare = args
+
+    do_set_comp = do_set_compare
+
+
+    def do_set_dataset( self, path ):
+        expanded_path = self.expand_path( path )
+        if not os.path.exists( expanded_path ):
+            self.error( "Dataset path does not exist: {}".format( expanded_path ) )
+            return
+        self.dataset = Dataset( expanded_path )
+
+    do_set_dataset_path = do_set_dataset
+    do_set_data_path = do_set_dataset
+
+
+    def do_dataset_suffix( self, args ):
+        if self.dataset.suffix( args ):
+            self.message( "Current dataset is: {}".format( self.dataset.data_path ) )
+        else:
+            self.error( "Could not change suffix" )
